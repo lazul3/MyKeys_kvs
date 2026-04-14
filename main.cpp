acfa6c66fe7b7ee8f03ca719a962cf391744a0ae
lazul3 <imela2811@gmail.com>
2026-04-13 23:51:19 -0600

    simple tokenizer working

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-  cout << "Mykeys\n";
-  return 0;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,33 @@
 #include <vector>
 
 using namespace std;
+enum Keyword {
+  SET,
+  GET,
+  DEL,
+  WORD,
+};
+struct Token {
+  Keyword type;
+  string value;
+};
+Keyword stringToKeyword(const string &str);
+Token makeToken(Keyword keyword, string value);
 
-vector<string> tokenize(string input) {
-  vector<string> tokens;
+vector<Token> tokenize(string input) {
+  vector<Token> tokens;
   string aux = "";
-  if (input.empty()){
-      cout << "Empty string";
+  Keyword key;
+  Token token;
+  if (input.empty()) {
+    cout << "Empty string";
   }
   for (char c : input) {
-    if (c == ' '|| c=='\n' || c== '\t') {
+    if (c == ' ' || c == '\n' || c == '\t') {
       if (!aux.empty()) {
-        tokens.push_back(aux);
+        key = stringToKeyword(aux);
+        token = makeToken(key, aux);
+        tokens.push_back(token);
         aux = "";
       }
 
@@ -20,16 +36,38 @@ vector<string> tokenize(string input) {
       aux = aux + c;
     }
   }
-  if (!aux.empty()){
-      tokens.push_back(aux);
+  if (!aux.empty()) {
+    key = stringToKeyword(aux);
+    token = makeToken(key, aux);
+    tokens.push_back(token);
   }
   return tokens;
 }
+
+Keyword stringToKeyword(const string &str) {
+  if (str == "SET") {
+    return Keyword::SET;
+  } else if (str == "GET") {
+    return Keyword::GET;
+  } else if (str == "DEL") {
+    return Keyword::DEL;
+  } else {
+    return Keyword::WORD;
+  }
+}
+
+Token makeToken(Keyword keyword, string value) {
+  Token t;
+  t.type = keyword;
+  t.value = value;
+  return t;
+}
+
 int main() {
-  string input = "";
-  vector<string> parseT = tokenize(input);
-  for (string i : parseT) {
-    cout << i << "\n";
+  string input = "SET key value";
+  vector<Token> parseT = tokenize(input);
+  for (Token i : parseT) {
+    cout << i.value << "\n";
   }
   return 0;
 }
