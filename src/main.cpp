809983961f88162df6fdc2972adea86d17c26093
lazul3 <imela2811@gmail.com>
2026-04-27 13:58:09 -0600

    parser for SET,GET,DEL & Unknown commands

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,23 @@ enum Keyword {
   DEL,
   WORD,
 };
+
+enum class ErrorCode {
+  OK = 0,
+  Emptystring,
+};
+
+struct SetCommand {
+  string key;
+  string value;
+};
+struct GetCommand {
+  string key;
+};
+struct DelCommand {
+  string key;
+};
+
 struct Token {
   Keyword type;
   string value;
@@ -20,15 +37,17 @@ vector<Token> tokenize(string input) {
   string aux = "";
   Keyword key;
   Token token;
+  // cout << input << "\n";
   if (input.empty()) {
-    cout << "Empty string";
+    cerr << "Empty String";
+    exit(1); // not ideal for REPL
   }
   for (char c : input) {
     if (c == ' ' || c == '\n' || c == '\t') {
       if (!aux.empty()) {
         key = stringToKeyword(aux);
         token = makeToken(key, aux);
-        tokens.push_back(token);
+        tokens.push_back(token); // add the token to the vector
         aux = "";
       }
 
@@ -36,7 +55,7 @@ vector<Token> tokenize(string input) {
       aux = aux + c;
     }
   }
-  if (!aux.empty()) {
+  if (!aux.empty()) { // for the last string
     key = stringToKeyword(aux);
     token = makeToken(key, aux);
     tokens.push_back(token);
@@ -63,11 +82,55 @@ Token makeToken(Keyword keyword, string value) {
   return t;
 }
 
-int main() {
-  string input = "SET key value";
-  vector<Token> parseT = tokenize(input);
-  for (Token i : parseT) {
-    cout << i.value << "\n";
+void parseToken(vector<Token>& parseT) {
+  size_t args = parseT.size();
+  if (parseT[0].type == Keyword::SET) {
+    if (args == 3) {
+      // cout << "SET \n";
+      SetCommand command;
+      command.key = parseT[1].value;
+      command.value = parseT[2].value;
+      cout << "SetCommand: key= " << command.key << ", value= " << command.value
+           << "\n";
+
+    } else {
+      cerr << "Error length";
+      return;
+    }
+  } else if (parseT[0].type == Keyword::GET) {
+    if (args == 2) {
+      // cout << "GET";
+      GetCommand command;
+      command.key = parseT[1].value;
+      cout << "GetCommand: key= " << command.key << "\n";
+
+    } else {
+      cerr << "Error length";
+      return;
+    }
+  } else if (parseT[0].type == Keyword::DEL) {
+    if (args == 2) {
+      // cout << "DEL";
+      DelCommand command;
+      command.key = parseT[1].value;
+      cout << "DelCommand: key= " << command.key << "\n";
+
+    } else {
+      cerr << "Error length";
+      return;
+    }
+  } else {
+    cerr << "Unknown command";
+    return;
   }
+  // for (Token i : parseT) {
+  //   cout << i.value << "\n";
+  // }
+}
+
+int main() {
+  string input = "GET age";
+  vector<Token> parseT = tokenize(input); // make the vector of tokens
+  parseToken(parseT);
   return 0;
 }
